dc334dddbdc14f1d96cf6b509b2ff7628a2225ae
Petrov Kirill <ottokirik@gmail.com>
2022-05-07 16:42:26 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Routes, Route } from 'react-router-dom'
+import { Header } from './components/header/header'
+import { HomePage } from './pages/home-page'
+import { LatestNewsPage } from './pages/latest-news-page'
+import { PopularNewsPage } from './pages/popular-news-page'
+
 function App() {
-  return <div>Инициализация</div>;
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/latest-news" element={<LatestNewsPage />} />
+        <Route path="/popular-news" element={<PopularNewsPage />} />
+      </Routes>
+    </div>
+  )
 }
 
-export default App;
+export default App
